f4b2d323b28f50dc6f46e38d637eb5b688852f7b
lceBoss <614695336@qq.com>
2021-03-16 11:46:15 +0800

    按钮

--- LCESwiftBaseProject/Controllers/HomeVC/LCEHomeViewController.swift
+++ LCESwiftBaseProject/Controllers/HomeVC/LCEHomeViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class LCEHomeViewController: LCEBaseViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -18,6 +18,22 @@ class LCEHomeViewController: LCEBaseViewController {
         self.naviView.navTitle = "首页"
         self.naviView.isHiddenBackButton = true
         
+        self.view .addSubview(self.button)
+        
     }
+    
+    @objc func touchButtonAction(sender: UIButton) -> Void {
+        print("哈哈哈哈")
+    }
+    
+    lazy var button: UIButton = {
+        let button = UIButton.init(type: .custom)
+        button.frame = CGRect(x: 0, y: 0, width: 160, height: 70)
+        button.center = self.view.center
+        button.setTitle("点击", for: .normal)
+        button.setTitleColor(.orange, for: .normal)
+        button.addTarget(self, action: #selector(touchButtonAction(sender:)), for: .touchUpInside)
+        return button
+    }()
 
 }
---
+++ LCESwiftBaseProject/Controllers/HomeVC/LCESwiftUI.swift
@@ -0,0 +1,23 @@
+//
+//  LCESwiftUI.swift
+//  LCESwiftBaseProject
+//
+//  Created by mac on 2020/12/8.
+//  Copyright © 2020 yaohuxiaozi. All rights reserved.
+//
+
+import SwiftUI
+
+struct LCESwiftUI: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct LCESwiftUI_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            LCESwiftUI()
+        }
+    }
+}
